c9ed38122d9e96d62998ccdac41635ee4262b025
CalisJI <buinguyenquythi@gmail.com>
2024-08-16 17:01:10 +0700

    full function game

--- src/main.cpp
+++ src/main.cpp
@@ -256,7 +256,7 @@ bool created_shot = false;
 bool direction = false; // false = left, true = right;
 int lastpoint_shot[2];
 unsigned long period_shot = millis();
-int speed_shot = 100;
+int speed_shot = 50;
 void Drawbullet(bool deleted = false)
 {
   // measure x dimension
@@ -430,50 +430,229 @@ void Defend()
     }
   }
 }
+
+bool mode = false;
+int reset_stickman = 2000;
+bool Mode_game = true;
+int width = 26;
+int height = 26;
+int X_craft = 0;
+int Y_craft = 0;
+bool exist = false;
+const int num_rocket = 15;
+int rockets[num_rocket][4] =
+    {
+        {0, -2, 0, -2},
+        {0, -14, 0, -14},
+        {0, -26, 0, -26},
+        {0, -38, 0, -38},
+        {0, -50, 0, -50},
+        {0, -62, 0, -62},
+        {0, -74, 0, -74},
+        {0, -86, 0, -86},
+        {0, -98, 0, -98},
+        {0, -110, 0, -110}};
+const int enemies_numX = 15;
+const int enemies_numY = 3;
+const int enemies_num = enemies_numX * enemies_numY;
+int enemies[enemies_num][2];
+int x_step = 8;
+int y_step = 6;
+int live_enemies = 0;
+void create_enemies()
+{
+  live_enemies = enemies_num;
+  for (int i = 1; i < enemies_numX + 1; i++)
+  {
+    for (int j = 1; j < enemies_numY + 1; j++)
+    {
+      int x = i * x_step;
+      int y = j * y_step;
+
+      enemies[(i - 1) * 3 + (j - 1)][0] = x;
+      enemies[(i - 1) * 3 + (j - 1)][1] = y;
+    }
+  }
+}
+
+void draw_enemies()
+{
+  for (int i = 0; i < enemies_num; i++)
+  {
+    uint16_t color = 0x03FF;
+    int x = enemies[i][0];
+    int y = enemies[i][1];
+    if (x == 0 && y == 0)
+      continue;
+    dma_display->drawCircle(enemies[i][0], enemies[i][1], 1, color);
+  }
+}
+
+void destroy_enemy()
+{
+  if (!exist)
+    return;
+  for (int i = 0; i < enemies_num; i++)
+  {
+    for (int x = 0; x < num_rocket; x++)
+    {
+      int x1_rocket = rockets[x][0];
+      int y1_rocket = rockets[x][1];
+      int x2_rocket = rockets[x][2];
+      int y2_rocket = rockets[x][3];
+
+      int x_enemy = enemies[i][0];
+      int y_enemy = enemies[i][1];
+      bool check_init = x1_rocket!=0&&x2_rocket!=0&&x_enemy!=0&&y_enemy!=0;
+      if (((x1_rocket == x_enemy && y1_rocket + 100 == y_enemy) || (x2_rocket == x_enemy && y2_rocket + 100 == y_enemy)) && live_enemies > 0 && check_init)
+      {
+        dma_display->drawCircle(enemies[i][0], enemies[i][1], 1, 0x0000);
+        enemies[i][0] = 0;
+        enemies[i][1] = 0;
+        live_enemies--;
+        Serial.println(live_enemies);
+        break;
+      }
+    }
+  }
+  draw_enemies();
+}
+void draw_enemy(int x, int y)
+{
+  uint16_t color = 0x03FF;
+  int _x = x;
+  int _y = y;
+  if (_y <= 64)
+  {
+    dma_display->fillCircle(_x, _y, 1, color);
+  }
+  else
+  {
+    _y = _y - 64;
+    _x = _x + 128;
+
+    dma_display->drawPixel(_x, _y, color);
+  }
+}
+void draw_rocket(int x_craft, int y_craft = 100)
+{
+  unsigned long now = millis();
+  if (now - period_shot >= speed_shot)
+  {
+    period_shot = now;
+    // Serial.println("clear");
+    for (int row = 0; row < num_rocket; row++)
+    {
+      for (int col = 0; col < 2; col++)
+      {
+        int x = rockets[row][col * 2];
+        int y = rockets[row][col * 2 + 1] + y_craft;
+        // dma_display->drawPixel(x,y,0x0000);
+        uint16_t color = 0x0000;
+
+        int _x = x;
+        int _y = y;
+        if (_y <= 64)
+        {
+          dma_display->drawPixel(_x, _y, color);
+        }
+        else
+        {
+          _y = _y - 64;
+          _x = _x + 128;
+
+          dma_display->drawPixel(_x, _y, color);
+        }
+        rockets[row][col * 2 + 1] -= 1;
+        if (rockets[row][col * 2 + 1] <= -110)
+        {
+          rockets[row][col * 2 + 1] = -2;
+        }
+      }
+    }
+  }
+  // Draw
+  for (int row = 0; row < num_rocket; row++)
+  {
+    for (int col = 0; col < 2; col++)
+    {
+      if (rockets[row][col * 2 + 1] == -2)
+      {
+        rockets[row][0] = x_craft + 4;
+        rockets[row][2] = x_craft + 21;
+      }
+
+      int x = rockets[row][col * 2];
+      int y = rockets[row][col * 2 + 1] + y_craft;
+      if (y <= 0 || x == 0)
+        continue;
+      uint16_t color = 0xF9A0;
+
+      int _x = x;
+      int _y = y;
+      if (_y <= 64)
+      {
+        dma_display->drawPixel(_x, _y, color);
+      }
+      else
+      {
+        _y = _y - 64;
+        _x = _x + 128;
+
+        dma_display->drawPixel(_x, _y, color);
+      }
+    }
+  }
+}
+void draw_spacecraft(int x, int y = 100)
+{
+  for (int i = 0; i < width; i++)
+  {
+    for (int j = 0; j < height; j++)
+    {
+      uint16_t color = aircraft[j * width + i];
+      if (color == 0xFFFF || color == 0xFFDF || color == 0xFFFE || color == 0xFFBD || color == 0xFFBF)
+        color = 0x0000;
+      int _x = x + i;
+      int _y = y + j;
+      if (_y <= 64)
+      {
+        dma_display->drawPixel(_x, _y, color);
+      }
+      else
+      {
+        _y = _y - 64;
+        _x = _x + 128;
+
+        dma_display->drawPixel(_x, _y, color);
+      }
+    }
+  }
+
+  exist = true;
+  // draw_rocket(x,y);
+}
 void setup()
 {
   Serial.begin(115200 * 2); // Đảm bảo baud rate khớp với baud rate của Python
   dma_display = new MatrixPanel_I2S_DMA(mxconfig);
-  dma_display->begin();            // setup the LED matrix
+  dma_display->begin();             // setup the LED matrix
   dma_display->setBrightness8(200); // 0-255
   dma_display->clearScreen();
+  // draw_spacecraft(32, 32);
+  Mode_game = true;
+  // create_enemies();
+  // draw_enemies();
+  // draw_enemy(32,32);
+  // draw_rocket(32,100);
   // draw_cat(10,10,20,20);
   // drawXbm565(10,10,20,20,CAT_bits,dma_display->color565(100,0,0));
-}
-
-bool mode = false;
-int reset_stickman = 2000;
-bool Mode_game = false;
-
-void draw_spacecraft(int x, int y){
-    for (int i = 0; i < 20; i++)
-    {
-        for (int j = 0; j < 20; j++)
-        {
-            uint16_t color = aircraft[j*20+i];
-            int _x = x+i;
-            int _y = y+i;
-            if (_y <= 64)
-            {
-              dma_display->drawPixel(_x, _y, color);
-            }
-            else
-            {
-              _y = _y - 64;
-              _x = _x + 128;
-
-              dma_display->drawPixel(_x, _y, color);
-            }
-            dma_display->drawPixel(_X,_y,color);
-        }
-        
-    }
-    
 }
 
 void loop()
 {
   // server.handleClient();
+
 #pragma region Serial processing
   while (Serial.available() > 0)
   {
@@ -500,16 +679,16 @@ void loop()
     if (numRows == 1 && numCols == 0)
     {
       Mode_game = true;
-      //dma_display->clearScreen();
-      // Serial.println("Stick Man");
+      // dma_display->clearScreen();
+      //  Serial.println("Stick Man");
     }
     else if (numRows == 0 && numCols == 0)
     {
       Mode_game = false;
       // Serial.println("HungVat");
-      //dma_display->clearScreen();
+      // dma_display->clearScreen();
     }
-    
+
     int expectedSize = numRows * numCols * floatSize;
     if (bufferIndex >= (2 * sizeof(int) + expectedSize))
     {
@@ -532,33 +711,28 @@ void loop()
       }
       else
       {
-        if (!game_over)
-          drawStickMan(int(data[0][0]), int(data[0][1]), int(data[0][2]));
+        X_craft = int(data[0][0]);
+        draw_spacecraft(X_craft);
+        draw_rocket(X_craft);
       }
 
       // Reset chỉ số chỉ mục sau khi xử lý
-      
     }
     bufferIndex = 0;
   }
-  if (Mode_game)
-  {
-    if (game_over)
-    {
-      if (millis() - period_gameover >= reset_stickman)
-      {
-        game_over = false;
-        dma_display->clearScreen();
-
-      }
-    }
-  }
-
   // Stickman region
   if (Mode_game)
   {
-    Drawbullet();
-    Defend();
+    if (exist)
+    {
+      draw_rocket(X_craft);
+      if (live_enemies <= 0)
+      {
+        create_enemies();
+      }
+      draw_enemies();
+      destroy_enemy();
+    }
   }
   else // Hung Vat
   {
@@ -570,73 +744,3 @@ void loop()
 
   // readDataFromSerial();
 }
-
-// int x = int(data[row][0]);
-// int y = int(data[row][1]);
-// int x_in = int(data[0][0]);
-// int y_in = int(data[0][1]);
-
-// if (x_in == 0 && y_in == 0)
-// {
-//   mode = true;
-// }
-// else if (x_in == 255 && y_in == 255)
-// {
-//   mode = false;
-// }
-// if (mode)
-// {
-//   int *_range;
-//   _range = drawStickMan(x, y);
-// }
-// else
-// {
-//   // measure x dimension
-//   int min_x = x - 1 - ((num_moi * 3) / 3);
-//   int max_x = x + 1 + ((num_moi * 3) / 3);
-//   int min_y = y - 1;
-//   int max_y = y + 1;
-//   for (int i = min_x; i <= max_x; i++)
-//   {
-//     for (int j = y - 1; j <= y + 1; j++)
-//     {
-//       if (j <= 32)
-//         dma_display->drawPixelRGB888(i, j, 214, 24, 192);
-//       else
-//         dma_display->drawPixelRGB888(i, j, 214, 192, 24);
-//       // reach moi
-//       if (gen_moi == false && y<255)
-//       {
-//         drawRandomPoint(x, y, 5 + (num_moi * 3), false);
-
-//         gen_moi = true;
-//       }
-//       if (min_x <= x_moi + 1 && max_x > x_moi - 1 && min_y <= y_moi + 1 && max_y >= y_moi - 1)
-//       {
-//         reached = true;
-//         num_moi += 1;
-//         gen_moi = false;
-//       }
-//     }
-//   }
-//   if (reached)
-//   {
-//     min_x = x - 1 - ((num_moi * 3) / 3);
-//     max_x = x + 1 + ((num_moi * 3) / 3);
-//     for (int i = min_x; i <= max_x; i++)
-//     {
-//       for (int j = y - 1; j <= y + 1; j++)
-//       {
-//         if (j <= 32)
-//           dma_display->drawPixelRGB888(i, j, 214, 24, 192);
-//         else
-//           dma_display->drawPixelRGB888(i, j, 214, 192, 24);
-//       }
-//     }
-//     reached = false;
-//   }
-//   if (gen_moi)
-//   {
-//     drawRandomPoint(x, y, 5 + (num_moi * 3), gen_moi);
-//   }
-// }
